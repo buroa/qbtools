afd5e6f8805191bb53cba5f910a5441f29ac6803
Devin Buhl <onedr0p@users.noreply.github.com>
2024-10-08 17:50:35 -0400

    feat: add qb/sab limit script (#84)
    Co-authored-by: Steven Kreitzer <skre@skre.me>

---
+++ qbtools/commands/limiter.py
@@ -0,0 +1,134 @@
+import time
+import requests
+
+from qbtools import utils
+from decimal import Decimal
+from typing import Optional, Tuple
+
+
+def __init__(app, logger):
+    logger.info("Starting limiter process...")
+
+    def process():
+        qbittorrent_queue, qbittorrent_current_limit = qbittorrent_data(app)
+        sabnzbd_queue, sabnzbd_current_limit = sabnzbd_data(app)
+
+        logger.debug(
+            f"qBittorrent: {qbittorrent_queue} item(s) @ {qbittorrent_current_limit} MB/s"
+        )
+        logger.debug(
+            f"SabNZBD: {sabnzbd_queue} item(s) @ max {sabnzbd_current_limit} MB/s"
+        )
+
+        percentage = (
+            app.limit_percent
+            if qbittorrent_queue and sabnzbd_queue
+            else app.max_percent
+        )
+
+        limit = int(app.max_line_speed_mbps * percentage)
+
+        if qbittorrent_current_limit != limit:
+            app.client.transfer_set_download_limit(
+                limit * 1024 * 1024
+            )
+            logger.info(
+                f"qbittorrent download limit set to {limit} MB/s "
+                f"(was {qbittorrent_current_limit} MB/s)..."
+            )
+
+        if sabnzbd_current_limit != limit:
+            handle_request(
+                url=f"{app.sabnzbd_host}/api",
+                method="POST",
+                data=dict(
+                    apikey=app.sabnzbd_apikey,
+                    mode="config",
+                    name="speedlimit",
+                    value=limit,
+                ),
+            )
+            logger.info(
+                f"sabnzbd download limit set to {limit} MB/s "
+                f"(was {sabnzbd_current_limit} MB/s)..."
+            )
+
+    while True:
+        try:
+            process()
+        except Exception as e:
+            logger.error(e)
+
+        time.sleep(app.interval)
+
+
+def qbittorrent_data(app) -> Tuple[int, int]:
+    torrents = len(app.client.torrents.info(status_filter="downloading"))
+    download_limit = app.client.transfer_download_limit() / 1024 / 1024
+    return torrents, download_limit
+
+
+def sabnzbd_data(app) -> Tuple[int, int]:
+    data = handle_request(
+        f"{app.sabnzbd_host}/api?apikey={app.sabnzbd_apikey}&mode=queue&output=json"
+    )
+    return (
+        int(data.get("queue", {}).get("noofslots", 0)) if data else 0,
+        int(data.get("queue", {}).get("speedlimit", 0)) if data else 0,
+    )
+
+
+def handle_request(
+    url: str, method: str = "GET", data: Optional[dict] = None
+) -> Optional[dict]:
+    response = requests.post(url, data=data) if method == "POST" else requests.get(url)
+    response.raise_for_status()
+    return response.json() if method == "GET" else None
+
+
+def add_arguments(command, subparser):
+    """
+    Description:
+        Limit speeds when qBittorrent and SabNZBD are both downloading.
+    Usage:
+        qbtools.py limiter --help
+    """
+    parser = subparser.add_parser(command)
+    parser.add_argument(
+        "--sabnzbd-host",
+        action=utils.EnvDefault,
+        envvar="SABNZBD_HOST",
+        help="The host of the SabNZBD server",
+        required=True,
+    )
+    parser.add_argument(
+        "--sabnzbd-apikey",
+        action=utils.EnvDefault,
+        envvar="SABNZBD_API_KEY",
+        help="The API key for the SabNZBD server",
+        required=True,
+    )
+    parser.add_argument(
+        "--max-line-speed-mbps",
+        type=float,
+        default=100,
+        help="The maximum line speed in Mbps",
+    )
+    parser.add_argument(
+        "--limit-percent",
+        type=float,
+        default=0.5,
+        help="The percentage of the line speed to limit to when both are downloading",
+    )
+    parser.add_argument(
+        "--max-percent",
+        type=float,
+        default=1.0,
+        help="The maximum percentage of the line speed to limit to when both are not downloading",
+    )
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=5,
+        help="The interval to check the speeds in seconds",
+    )
